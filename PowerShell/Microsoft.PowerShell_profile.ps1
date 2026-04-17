4b341e49f9c548d7cf2e333856688b5aeed01e01
aSumo <1xtelescope@gmail.com>
2026-04-17 19:01:18 +0900

    refactor: 改行を調整

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,10 @@
 Import-Module PSReadline
-Set-PSReadLineOption -EditMode Emacs
-
 Invoke-Expression (&starship init powershell)
 
 $script:Abbr = @{}
 . "$HOME\Documents\PowerShell\pwsh_abbr.ps1"
 Register-AbbrHandler
 . "$HOME\Documents\PowerShell\pwsh_alias.ps1"
-""
-"Now initialized."
+
+Write-Host ""
+Write-Host "Now initialized."
